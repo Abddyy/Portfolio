86c9e95a3ca09bdca89a8c9467d01e950f0dd53a
Abdallah Yahya <yabood358@gmail.com>
2024-07-13 02:46:23 +0300

    WIP

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -5,5 +5,5 @@ window.addEventListener('scroll', function() {
     const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
     const alpha = Math.min(1, (scrollPosition / (maxScroll / 2))); // Adjust the divisor to make it faster
 
-    navbar.style.backgroundColor = `rgba(31, 31, 31, ${alpha})`;
+    navbar.style.backgroundColor = `rgba(34, 40, 49, ${alpha})`;
 });
--- scripts/typeParagraph.js
+++ scripts/typeParagraph.js
@@ -1,3 +1,16 @@
+// Function to type heading text
+function typeHeadingText(element, text, index, speed) {
+    if (index < text.length) {
+        element.innerHTML = text.substring(0, index + 1) + '<span class="cursor"><b>|</b></span>';
+        setTimeout(() => {
+            typeHeadingText(element, text, index + 1, speed);
+        }, speed);
+    } else {
+        element.innerHTML = text; // Display the text and remove the cursor completely
+    }
+}
+
+// Function to type paragraph text
 function typeParagraphText(element, text, index, speed, initialDelay) {
     setTimeout(() => {
         if (index < text.length) {
@@ -12,6 +25,7 @@ function typeParagraphText(element, text, index, speed, initialDelay) {
     }, initialDelay);
 }
 
+// Function to blink the cursor
 function blinkCursor(cursor) {
     setInterval(() => {
         cursor.style.opacity = (cursor.style.opacity == 0 ? 1 : 0);
@@ -19,10 +33,14 @@ function blinkCursor(cursor) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    const headingText = "Hi There";
+    const headingElement = document.querySelector('.biography h3');
+    typeHeadingText(headingElement, headingText, 0, 60); // Set the speed for typing the heading
+
     const paragraphText = `My name is Abdullah Yahya, a 20 years old CS graduate at NNU,
     passionate about programming in different areas, fast learner
     and can work within a team of programmers. Excited to apply my
     skills and gain real-world experience while making a positive impact.`;
     const paragraphElement = document.querySelector('.biography p');
-    typeParagraphText(paragraphElement, paragraphText, 0, 20, 1000); // Faster typing speed and delayed start
+    typeParagraphText(paragraphElement, paragraphText, 0, 17, 1000); // Faster typing speed and delayed start
 });
