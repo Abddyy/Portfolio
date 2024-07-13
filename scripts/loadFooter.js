e123e8e987a8028979977e60ff062a11a88583e6
Abdallah Yahya <yabood358@gmail.com>
2024-07-13 05:19:32 +0300

    WIP

---
+++ scripts/loadFooter.js
@@ -0,0 +1,16 @@
+// loadFooter.js
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading footer:', error);
+        });
+});
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     const scrollPosition = window.scrollY;
     const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-    const alpha = Math.min(1, (scrollPosition / (maxScroll / 2))); // Adjust the divisor to make it faster
+    const alpha = Math.min(1, (scrollPosition / (maxScroll / 2))); 
 
     navbar.style.backgroundColor = `rgba(34, 40, 49, ${alpha})`;
 });
