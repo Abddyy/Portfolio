df55e82edf5538909c3eedebdd7e8c2c427c2232
Abdallah Yahya <yabood358@gmail.com>
2024-07-24 01:13:14 +0300

    wip

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,35 +1,43 @@
-function showLoading() {
-    console.log('Showing loading GIF');
-    document.getElementById('loading').style.display = 'flex';
-}
-
-function hideLoading() {
-    console.log('Hiding loading GIF');
-    document.getElementById('loading').style.display = 'none';
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOMContentLoaded event fired');
-    const links = document.querySelectorAll('a');
+    const links = document.querySelectorAll('.nav-link');
+    const mainContent = document.querySelector('main');
+    const spinner = document.getElementById('loading-spinner');
 
     links.forEach(link => {
         link.addEventListener('click', function (event) {
-            const href = link.getAttribute('href');
+            event.preventDefault();
+            const url = this.href;
 
-            if (href && !href.startsWith('#') && !link.classList.contains('no-loading') && link.getAttribute('target') !== '_blank') {
-                showLoading();
+            console.log(`Fetching ${url}`);
+            spinner.classList.remove('d-none'); // Show the loading spinner
 
-                setTimeout(() => {
-                    window.location.href = href;
-                }, 100); 
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`Network response was not ok ${response.statusText}`);
+                    }
+                    return response.text();
+                })
+                .then(html => {
+                    console.log(`Successfully fetched ${url}`);
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(html, 'text/html');
+                    const newContent = doc.querySelector('main').innerHTML;
 
-                event.preventDefault();
-            }
+                    mainContent.innerHTML = newContent; // Replace the content
+                    window.history.pushState(null, null, url); // Update the URL
+                })
+                .catch(error => {
+                    console.error('Error fetching the page:', error);
+                })
+                .finally(() => {
+                    spinner.classList.add('d-none'); // Hide the loading spinner
+                });
         });
     });
 
-    window.addEventListener('load', function() {
-        console.log('Window load event fired');
-        hideLoading();
+    // Handle back/forward browser navigation
+    window.addEventListener('popstate', function () {
+        location.reload();
     });
 });
